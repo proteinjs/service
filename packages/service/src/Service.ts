a5e5e07806eeb958fcbe65f1ae2f33be97aae792
Veronica Valenzuela <vmvalenzuela24@gmail.com>
2024-05-10 13:36:30 -0700

    fix: add linting and lint all files

--- packages/service/src/Service.ts
+++ packages/service/src/Service.ts
@@ -6,21 +6,21 @@ export interface Service extends Loadable {
   serviceMetadata?: {
     auth?: {
       /** If true, the user does not need to be logged in or have any roles to call this service. If blank, defaults to false. */
-      public?: boolean,
+      public?: boolean;
       /** If true, the user does not need to have any roles to call this service, but must be logged in. If blank, defaults to false. */
-      allUsers?: boolean,
+      allUsers?: boolean;
       /** The user must be logged in and have these roles to call this service. If blank, defaults to requiring the 'admin' role. */
-      roles?: string[],
-      /** 
+      roles?: string[];
+      /**
        * Custom auth function. If provided, all other auth properties are ignored.
        * @param methodName the name of the service method to be executed
        * @param args the args[] that will be passed into the method
        * @return true if the user can access the service method
        */
-      canAccess?: (methodName: string, args: any[]) => boolean,
-    },
+      canAccess?: (methodName: string, args: any[]) => boolean;
+    };
     /** Don't await the service's execution, return a response to the client immediately */
-    doNotAwait?: boolean,
+    doNotAwait?: boolean;
   };
   [prop: string]: SerializableFunction | NotFunction<any>;
 }
@@ -31,16 +31,16 @@ export interface Service extends Loadable {
  * @param serviceInterfaceQualifiedName the package-qualified name of the service interface (ie. service-package-name/MyService)
  * @returns a function that creates a Service
  */
-export const serviceFactory = <T extends Service>(serviceInterfaceQualifiedName: string): () => T => {
+export const serviceFactory = <T extends Service>(serviceInterfaceQualifiedName: string): (() => T) => {
   return () => {
     const service: any = {};
     const serviceInterface = SourceRepository.get().interface(serviceInterfaceQualifiedName);
-    for (let method of serviceInterface.methods) {
+    for (const method of serviceInterface.methods) {
       const servicePath = `/service/${serviceInterface.qualifiedName}/${method.name}`;
       const serviceClient = new ServiceClient(servicePath);
       service[method.name] = serviceClient.send.bind(serviceClient);
     }
 
     return service;
-  }
-}
+  };
+};
--- packages/service/src/ServiceRouter.ts
+++ packages/service/src/ServiceRouter.ts
@@ -6,7 +6,7 @@ import { Logger, isInstanceOf } from '@proteinjs/util';
 
 export class ServiceRouter implements Route {
   private logger = new Logger(this.constructor.name);
-  private serviceExecutorMap: {[path: string]: ServiceExecutor}|undefined;
+  private serviceExecutorMap: { [path: string]: ServiceExecutor } | undefined;
   path = 'service/*';
   method: 'post' = 'post';
 
@@ -14,13 +14,14 @@ export class ServiceRouter implements Route {
     if (!this.serviceExecutorMap) {
       this.serviceExecutorMap = {};
       const serviceTypes = Object.values(SourceRepository.get().directChildren('@proteinjs/service/Service'));
-      for (let serviceType of serviceTypes) {
+      for (const serviceType of serviceTypes) {
         this.logger.info(`Loading service: ${serviceType.qualifiedName}`);
-        if (!isInstanceOf(serviceType, Interface))
+        if (!isInstanceOf(serviceType, Interface)) {
           continue;
+        }
 
         const service = SourceRepository.get().object<Service>(serviceType.qualifiedName);
-        for (let method of (serviceType as Interface).methods) {
+        for (const method of (serviceType as Interface).methods) {
           const servicePath = `/service/${serviceType.qualifiedName}/${method.name}`;
           this.serviceExecutorMap[servicePath] = new ServiceExecutor(service, serviceType as Interface, method);
         }
@@ -31,20 +32,20 @@ export class ServiceRouter implements Route {
   }
 
   async onRequest(request: any, response: any): Promise<any> {
-      const serviceExecutor = this.getServiceExecutorMap()[request.path];
-      if (!serviceExecutor) {
-        const error = `Unable to find service matching path: ${request.path}`;
-        this.logger.error(error);
-        response.send({ error });
-        return;
-      }
+    const serviceExecutor = this.getServiceExecutorMap()[request.path];
+    if (!serviceExecutor) {
+      const error = `Unable to find service matching path: ${request.path}`;
+      this.logger.error(error);
+      response.send({ error });
+      return;
+    }
 
-      try {
-        const serializedReturn = await serviceExecutor.execute(request.body);
-        response.send({ serializedReturn });
-      } catch (error: any) {
-        this.logger.error(error.stack);
-        response.send({ error: error.message });
-      }
+    try {
+      const serializedReturn = await serviceExecutor.execute(request.body);
+      response.send({ serializedReturn });
+    } catch (error: any) {
+      this.logger.error(error.stack);
+      response.send({ error: error.message });
+    }
   }
 }
